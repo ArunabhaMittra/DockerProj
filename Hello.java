f91c01a6662454e0be785452a4c2a74afcabfe43
First Last <arunabhamita97g@ip-172-31-67-211.ec2.internal>
2021-07-19 05:17:12 +0000

    Java App Version 2 - Committed Java App v2

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello {
   public static void main(String[] args) {
     System.out.println("This is a java app created using Docker!!");
+    System.out.println("This is version 2 of the java app!!");
   }
 }
