dd8c90775ba31b3b84f834e5300e554cd5e1f103
ArunabhaMittra <arunabhamita97g@ip-172-31-67-211.ec2.internal>
2021-07-20 04:51:02 +0000

    Third Commit - Version 3 of the Java App

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ class Hello {
   public static void main(String[] args) {
     System.out.println("This is a java app created using Docker!!");
     System.out.println("This is version 2 of the java app!!");
+    System.out.println("This line was added to showcase version 3 of the java app");
   }
 }
